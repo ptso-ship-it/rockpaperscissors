d34903f4dcabb47b6a1113729724f4f913ef52d2
ptso <prestonmtso@gmail.com>
2022-04-16 16:41:57 -0700

    computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+function computerPlay(){
+	let choice = Math.floor(Math.random() * 3+1);
+	let result = "Test";
+	if (choice === 1) {
+		result = "Rock";
+	} else if (choice === 2) {
+		result = "Paper";
+	} else {
+		result = "Scissors";
+	}
+	return result
+}
+
+let PlayerChoice = prompt("Enter your choice", "")
+
+
+
+console.log(computerPlay())
